667d9eecaaa8f29618f5a791caf05c6748e70dc2
neutronstar-ss <neutron.star.system.services@gmail.com>
2020-06-20 09:53:17 -0300

    first commit

---
+++ src/main/java/com/fresa/FresaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FresaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FresaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fresa/FresaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FresaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
